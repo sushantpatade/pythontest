0bb3841dab626657f14e9e288de82f7faf991c16
spatade@digite.com <spatade@digite.com>
2021-08-01 16:22:43 +0530

    fist commit and push 2nd

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("Inside main method")
+    print("2nd branch created")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
